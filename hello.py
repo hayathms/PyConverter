b2b679676ddcf8fa461e2b6bf4c5a22e8354ae28
Hayath.M.S <hayathms@gmail.com>
2012-04-24 08:08:24 +0530

    Git Testing

--- hello.py
+++ hello.py
@@ -1,5 +1,12 @@
 import tkinter
 from tkinter import ttk
+from tkinter import filedialog
+
+
+countrycodes = ('ar', 'au', 'be', 'br', 'ca', 'cn', 'dk', 'fi', 'fr', 'gr', 'in', 'it', 'jp', 'mx', 'nl', 'no', 'es', 'se', 'ch')
+countrynames = ('Argentina', 'Australia', 'Belgium', 'Brazil', 'Canada', 'China', 'Denmark', \
+                'Finland', 'France', 'Greece', 'India', 'Italy', 'Japan', 'Mexico', 'Netherlands', 'Norway', 'Spain', \
+                'Sweden', 'Switzerland')
 
 
 class CreateWindow:
@@ -9,35 +16,78 @@ class CreateWindow:
 
         self.root = tkinter.Tk()
         frame = self.mainframe()
+        first = FirstFrame(frame,0,0)
+        first.first_frame()
         mid = MiddleFrame(frame,1,0)
         mid.middle_frame()
+
+
+
         self.root.mainloop()
 
 
     def mainframe(self):
         '''This is The Method Which start building GUI'''
 
-        frame = ttk.Frame(self.root, width='600px', height='300px', borderwidth=2, relief='sunken')
-        frame.grid(row=0, column=0, sticky='NSEW')
+        frame = ttk.Frame(self.root,
+                          width='600px',
+                          height='300px',
+                          borderwidth=2,
+                          relief='sunken',
+                          padding=10)
+        frame.grid(row=0, column=0, sticky='NSEW', rowspan=2)
         frame.columnconfigure(0, weight=1)
         frame.rowconfigure(0, weight=1)
-        self.buttons(frame)
-        self.entries(frame)
         return frame
         
 
-    def buttons(self, mainframe):
-        '''Here comes all Mainframe Buttons''' 
+class FirstFrame:
+    '''This class Initialised Parameters for First Frame'''
 
-        browse_button = ttk.Button(mainframe, command=exit, text = 'Browse')
+
+    def __init__(self, mainframe, column, row):
+        '''Initialting Middle Frame'''
+
+        self.mainframe = mainframe
+        self.column = column
+        self.row = row
+
+
+    def first_frame(self):
+        '''Craeating Middle Frame'''
+
+        frame = ttk.Frame(self.mainframe,
+                          width='200px', 
+                          height='200px',
+                          borderwidth=2, 
+                          relief='sunken',
+                          padding=5)
+        frame.grid(row=self.row, column=self.column, sticky='NSEW', rowspan=2)
+        frame.columnconfigure(0, weight=1)
+        frame.rowconfigure(0, weight=1)
+        self.buttons(frame)
+        self.lbox(frame)
+
+
+    def buttons(self, mainframe):
+        '''Here comes all First Frame Buttons''' 
+
+        browse_button = ttk.Button(mainframe, command=open_file, text = 'Browse')
         browse_button.grid(row = 0, column=0, sticky='N')
 
 
-    def entries(self, mainframe):
+    def lbox(self, mainframe):
         '''Here comes the Entry Widgets'''
-        
-        enter_files = ttk.Entry(mainframe)
-        enter_files.grid(row=1, column=0)
+
+
+        cnames = tkinter.StringVar(value=countrynames)
+        listbox = tkinter.Listbox(mainframe, height=10, listvariable=cnames)
+        listbox.grid(column=0, row=1)
+        scroll = ttk.Scrollbar(mainframe, orient='vertical', command=listbox.yview)
+        listbox.configure(yscrollcommand=scroll.set)
+        scroll.grid(column=1, row=1, sticky='NS')
+        listbox['yscrollcommand'] = scroll.set
+        ttk.Sizegrip(mainframe).grid(column=1, row=2, sticky='SE')
 
 
 class MiddleFrame:
@@ -55,10 +105,40 @@ class MiddleFrame:
     def middle_frame(self):
         '''Craeating Middle Frame'''
 
-        frame = ttk.Frame(self.mainframe, width='200px', height='200px', borderwidth=2, relief='sunken')
-        frame.grid(row=self.row, column=self.column, sticky='NSEW')
-        frame.columnconfigure(0, weight=1)
-        frame.rowconfigure(0, weight=1)
+        frame = ttk.Frame(self.mainframe,
+                         width='200px',
+                         height='200px',
+                         borderwidth=2,
+                         relief='sunken',
+                         padding=5)
+        frame.grid(row=self.row,
+                   column=self.column,
+                   sticky='NSEW',
+                   rowspan=2)
+        frame.columnconfigure(self.column, weight=1)
+        frame.rowconfigure(self.row, weight=1)
+
+
+
+
+
+def open_file():
+
+    filename = filedialog.askopenfilename(multiple=True)
+
+
+
+
+    print (filename)
+
+   
+
+
+
+
+
+
+
 
 
 
